3b92b93665a1a92d65dcb9070b62bb0aa90d8b3a
Bumilib <103141505+Bumilib@users.noreply.github.com>
2024-03-10 16:47:57 -0400

    Removing static array of routes (not needed if you are using file based routing )

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,46 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import { routes } from 'vue-router/auto-routes'
 
-const route = [
-  {
-    path: '/',
-    name: 'Home',
-    component: () => import('../pages/index.vue'),
-    meta: {
-      requireAuth: true
-    }
-  },
-  {
-    path: '/login',
-    name: 'Login',
-    component: () => import('../pages/login.vue')
-  },
-  {
-    path: '/signup',
-    name: 'Signup',
-    component: () => import('../pages/signup.vue')
-  },
-]
-
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: routes
-})
-
-router.beforeEach(async (to, from, next) => {
-  if(to.matched.some(record => record.meta.requireAuth)) {
-    // Authentication check
-    const token = localStorage.getItem('token')
-
-    if(token) {
-      // check if token is valid 
-      return next()
-    }
-
-    return next('/login')
-  } 
-
-  next()
 })
 
 export default router
