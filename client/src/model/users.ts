f8215b6797888e434a7dc9ff86961e2929ce0e5d
Bumilib <103141505+Bumilib@users.noreply.github.com>
2024-03-14 04:41:59 -0400

    made more changes to user.ts,json

--- client/src/model/users.ts
+++ client/src/model/users.ts
@@ -1,29 +1,27 @@
 import user from '@/data/users.json';
-import { ref } from 'vue';
-
-export interface Hair {
-  color: string;
-  type: string;
-}
+import { reactive, ref } from 'vue';
 
 export interface User {
-  id: number;
-  firstName: string;
-  lastName: string;
-  maidenName: string;
-  age: number;
-  gender: string;
-  email: string;
-  phone: string;
-  username: string;
-  password: string;
-  birthDate: string;
-  image: string;
-  bloodGroup: string;
-  height: number;
-  weight: number;
-  eyeColor: string;
-  hair: Hair;
+  id: number
+  firstName: string
+  lastName: string
+  maidenName: string
+  age: number
+  gender: string
+  email: string
+  phone: string
+  username: string
+  password: string
+  isAdmin: boolean
+  image: string
+  
+  activityID: number
+  date: string
+  name: string
+  caloriesBurned: number
+  duration: number
+  activityImage: string
+  workout: string
 }
 
 export function getUsers(): User[] {
@@ -33,9 +31,28 @@ export function getUsers(): User[] {
 export function useUser() {
   const users = ref([] as User[])
   users.value = getUsers()
-
-  let isLoggedIn = ref(false);
+}
+  //doesnt work properly yet
+  /*let isLoggedIn = ref(false);
   let currentUser = ref(null as User | null);
 
   return { isLoggedIn, currentUser };
 }
+  */
+
+//login function
+export const storeUser = reactive({
+  users: getUsers()
+});
+
+export function deleteUser(username: string) {
+  const index = storeUser.users.findIndex(user => user.username === username);
+  if (index !== -1) {
+    storeUser.users.splice(index, 1);
+  }
+}
+
+
+//
+
+
