011edf763134e725f6ee0436083dd3f7f68da868
Bumilib <103141505+Bumilib@users.noreply.github.com>
2024-03-15 12:00:03 -0400

    deleted and and made store

--- client/src/viewModel/session.ts
+++
@@ -1,7 +0,0 @@
-
-import { getUsers, type User } from "@/model/users";
-import { ref } from "vue";
-import { useRouter } from "vue-router";
-
-const currentUser = ref()
-export const refCurrentUser = () =>  currentUser
