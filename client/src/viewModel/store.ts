8fd3c47cb0862968bdec163aefef202157dffbda
Bumilib <103141505+Bumilib@users.noreply.github.com>
2024-04-29 14:36:44 -0400

    api store

--- client/src/viewModel/store.ts
+++ client/src/viewModel/store.ts
@@ -1,9 +1,13 @@
 import { reactive } from "vue";
 import { type User } from "@/model/users";
+
+const API_URL = 'http://localhost:3000/';
+
 const store = ({
   state: reactive({
     user:{} as User,
-    token:''
+    token:'',
+    pic_url:'',
   }),
 
   getters: {
@@ -14,14 +18,20 @@ const store = ({
       return store.state.token;
     },
     getPicture(){
-      return store.state.user.pic_url;
+      return store.state.pic_url;
     }
   },
 
   mutations: {
-    authenticateUser(data:User,token:string){
-      store.state.user = data
-      store.state.token = token
+    authenticateUser(data:User,token:string, pic_url:string){
+      store.state.user = data;
+      store.state.token = token;
+      store.state.pic_url = API_URL + pic_url;
+    },
+    logoutUser(){
+      store.state.user = {} as User;
+      store.state.token = '';
+      store.state.pic_url = '';
     }
   },
 
